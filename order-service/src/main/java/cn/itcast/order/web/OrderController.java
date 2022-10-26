d31c757825bd9f19980d292a18ebb16db0bf38a9
XiaoXiaoyi <9298460+masteryiii@user.noreply.gitee.com>
2022-10-26 16:28:34 +0800

    RestTemplate初应用

---
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -0,0 +1,39 @@
+package cn.itcast.order.web;
+
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import cn.itcast.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+   @Autowired
+   private OrderService orderService;
+
+   @Autowired
+   private RestTemplate restTemplate;
+
+    @GetMapping("{orderId}")
+    public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+
+        // 1. 查询订单
+        Order order = orderService.queryOrderById(orderId);
+
+        // 2. 根据order中的userId远程调用user服务进行查询
+        // 2.1 拼凑url
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        // 2.2 使用RestTemplate发送Get请求
+        User user = restTemplate.getForObject(url, User.class);
+        // 3. 封装数据
+        order.setUser(user);
+        // 4. 返回order
+        return order;
+    }
+}
