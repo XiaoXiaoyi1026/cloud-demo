2bc54b289f9897dad13d901d531f02e21c4fd0e9
xiaoxiaoyi <xiaoxiaoyi1026@gmail.com>
2022-11-01 16:54:54 +0800

    download source

---
+++ feign-api/src/main/java/xyx/xiaoxiaoyi/FeignApplication.java
@@ -0,0 +1,16 @@
+package xyx.xiaoxiaoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaoxiaoyi
+ */
+@SpringBootApplication
+public class FeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class);
+    }
+
+}
